895f9a6b6cfc1a68dab6e4a5ef0adbd2f3c738c5
amin <106171699+aminishere@users.noreply.github.com>
2025-12-16 01:46:31 +0530

    Create CountingBits.cpp

---
+++ bit_manipulations/CountingBits.cpp
@@ -0,0 +1,11 @@
+//bit dp
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>bits(n+1);
+        for(int i=1;i<n+1;i++){
+            bits[i]=bits[i>>1]+(i&1); // dpending on computed ones
+        }
+        return bits;
+    }
+};
