6969a73d2e3fada268acc1266686178def7de652
amin <106171699+aminishere@users.noreply.github.com>
2025-12-15 20:52:49 +0530

    Create SingleNumber.cpp

---
+++ bit_manipulations/SingleNumber.cpp
@@ -0,0 +1,12 @@
+// keyword XOR
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int sum=0;
+        for(auto it: nums){
+            sum^=it;
+        }
+        return sum;
+    }
+};
+//lc 136
